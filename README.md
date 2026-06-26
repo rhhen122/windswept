38f125151307c795a67bbd9779fbe361c6ee484f
rhhen122 <rokyhenderson222@gmail.com>
2026-06-26 20:22:22 +1200

    Added README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# WindSwept Package Manager
+### Fast Localised Package Manager written in NIM
+
+Requirements:
+ - NIM & NIMPLE
+ - MAKE
+ - GIT
+
+Compile with:
+```bash
+make build
+```
--- commands.nim
+++ commands.nim
@@ -4,6 +4,6 @@ proc writeCommands() =
     -v/--version    | Displays Version Info
     -s/--setup      | Run Setup Process for WindSwept
     -i/--install    | Example (-i="package-name")
-    -b/--build      | Rebuilds windswept. Using -b=i You can add it to the /bin directory (Run in WindSwept project root)
+    -b/--build      | Rebuilds windswept using -b=i You can add it to the /bin directory (Run in WindSwept project root)
     -u/--update     | Updates WindSwept (Run in root of WindSwept repo)
     """
